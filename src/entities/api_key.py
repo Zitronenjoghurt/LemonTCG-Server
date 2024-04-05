f07647054b08036c0c36c4e3e6dd421c0448903e
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-04-06 00:34:32 +0200

    add script to generate api_keys

--- src/entities/api_key.py
+++ src/entities/api_key.py
@@ -1,3 +1,4 @@
+import uuid
 from src.database.collection import Collection
 from src.entities.database_entity import DatabaseEntity
 
@@ -6,3 +7,8 @@ class ApiKey(DatabaseEntity):
     UNIQUE_FIELDS = ["key"]
     key: str
     user: str
+
+    @staticmethod
+    def new(username: str) -> 'ApiKey':
+        key = str(uuid.uuid4()).replace('-', '')
+        return ApiKey(key=key, user=username)
